3405eb436a0303b8c21b66295e0650f5654e7c0e
Bruzhmelev Anton <bruzhmelev@gmail.com>
2017-12-11 22:37:32 +0300

    two tests added for small check

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -2,36 +2,37 @@
 
 namespace GildedRose.Console
 {
-    class Program
+    public class Program
     {
-        IList<Item> Items;
+        public IList<Item> Items;
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
+            Run();
+            System.Console.ReadKey();
+        }
 
+        private static void Run()
+        {
             var app = new Program()
-                          {
-                              Items = new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
-
-                          };
+            {
+                Items = new List<Item>
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Item
+                    {
+                        Name = "Backstage passes to a TAFKAL80ETC concert",
+                        SellIn = 15,
+                        Quality = 20
+                    },
+                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                }
+            };
 
             app.UpdateQuality();
-
-            System.Console.ReadKey();
-
         }
 
         public void UpdateQuality()
@@ -110,6 +111,11 @@ namespace GildedRose.Console
             }
         }
 
+        public virtual void SetItems(List<Item> items)
+        {
+            
+        }
+        
     }
 
     public class Item
---
+++ src/GildedRose.Tests/Tests/WhenUpdateQuality.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using GildedRose.Console;
+using NUnit.Framework;
+
+namespace GildedRose.Tests.Tests
+{
+    [TestFixture]
+    public class WhenUpdateQuality
+    {
+        [Test]
+        public void IfItsNormalItem_ThenMakeLowerBothValuesForEveryItem()
+        {
+            var app = new Program()
+            {
+                Items = new List<Item>
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20}
+                }
+            };
+
+            app.UpdateQuality();
+            
+            Assert.AreEqual(9, app.Items[0].SellIn);
+            Assert.AreEqual(19, app.Items[0].Quality);
+        }
+        
+        [Test]
+        public void IfUpdetedOneHundredTimes_NotNegative()
+        {
+            var app = new Program()
+            {
+                Items = new List<Item>
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20}
+                }
+            };
+            
+            for (var i = 0; i < 100; i++)
+            {
+                app.UpdateQuality();
+            }
+            
+            Assert.IsTrue(app.Items[0].Quality >= 0, $"Expected {app.Items[0].Quality} >= 0");
+        }
+    }
+}
